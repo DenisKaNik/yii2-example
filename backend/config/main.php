f2c429d236aee2fc6aac9c60deea64618c5841de
dk <deniska.nik@gmail.com>
2020-03-03 11:18:15 +0700

    Added protection for admin panel

--- backend/config/main.php
+++ backend/config/main.php
@@ -45,5 +45,15 @@ return [
             ],
         ],
     ],
+    'as access' => [
+        'class' => 'yii\filters\AccessControl',
+        'except' => ['site/login', 'site/error'],
+        'rules' => [
+            [
+                'allow' => true,
+                'roles' => ['admin', 'operator'],
+            ],
+        ],
+    ],
     'params' => $params,
 ];
