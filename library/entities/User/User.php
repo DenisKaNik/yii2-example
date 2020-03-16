67885016b417ce96c1c5d3b2511234b5064549cc
dk <deniska.nik@gmail.com>
2020-03-16 14:24:22 +0700

    Added model `User`: entity, helper, read repository

---
+++ library/entities/User/User.php
@@ -0,0 +1,47 @@
+<?php
+namespace library\entities\User;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+/**
+ * User model
+ *
+ * @property integer $id
+ * @property string $username
+ * @property string $password_hash
+ * @property string $password_reset_token
+ * @property string $email
+ * @property string $email_confirm_token
+ * @property string $phone
+ * @property string $auth_key
+ * @property integer $status
+ * @property integer $created_at
+ * @property integer $updated_at
+ * @property string $password write-only password
+ *
+ */
+class User extends ActiveRecord
+{
+    const STATUS_WAIT = 0;
+    const STATUS_ACTIVE = 10;
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%user}}';
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return Yii::$app->security->validatePassword($password, $this->password_hash);
+    }
+}
